2afa8a582f2470aeb0c60e80b441b0e190c1f6f8
Aholic <aholic.12.05@gmail.com>
2025-02-07 12:17:07 +0800

    第一次提交

---
+++ Assets/Scripts/GitTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GitTest
+{
+	public class GitTest : MonoBehaviour
+	{
+		private string str = "First";
+	}
+}
+
